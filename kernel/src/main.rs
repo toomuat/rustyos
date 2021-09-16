3b0b002d872b808b73e20c42169967f7d10c4b92
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2021-09-16 21:25:55 +0900

    update kernel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(asm)]
+#![feature(lang_items)]
+
+mod graphics;
+
+use core::panic::PanicInfo;
+use graphics::{FrameBuffer, ModeInfo};
+
+#[no_mangle]
+extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
+
+#[lang = "eh_personality"]
+fn eh_personality() {}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
 }
