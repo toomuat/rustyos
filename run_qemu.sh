64c54eafcf418990139e93308101dccc8f931708
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2021-09-16 20:42:19 +0900

    update run script

--- run_qemu.sh
+++ run_qemu.sh
@@ -6,9 +6,32 @@ mkdir -p mnt/EFI/BOOT/
 
 cp $LOADER mnt/EFI/BOOT/BOOTX64.EFI
 
-qemu-system-x86_64 \
-   -drive if=pflash,format=raw,readonly,file=./OVMF/OVMF_CODE.fd \
-   -drive if=pflash,format=raw,file=./OVMF/OVMF_VARS.fd \
-   -drive format=raw,file=fat:rw:./mnt \
-   -chardev stdio,mux=on,id=com1 \
-   -serial chardev:com1
+QEMU_OPT="
+    -drive if=pflash,format=raw,readonly,file=./OVMF/OVMF_CODE.fd \
+    -drive if=pflash,format=raw,file=./OVMF/OVMF_VARS.fd \
+    -drive format=raw,file=fat:rw:./mnt"
+
+for OPT in "$@"
+do
+case $OPT in
+    "--serial")
+        # echo serial
+        QEMU_OPT="${QEMU_OPT} \
+        -chardev stdio,mux=on,id=com1 \
+        -serial chardev:com1"
+        break
+        ;;
+    "--monitor")
+        # echo monitor
+        QEMU_OPT="${QEMU_OPT} \
+        -monitor stdio"
+        break
+        ;;
+    *)
+        ;;
+esac
+shift
+done
+
+# echo $QEMU_OPT
+qemu-system-x86_64 $QEMU_OPT
