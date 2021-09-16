e5033335cf9fef17b499ca0693c532634749f61e
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2021-09-16 18:46:04 +0900

    add efi_main

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+use uefi::prelude::*;
+use uefi_services;
+
+#[entry]
+fn efi_main(image: Handle, mut st: SystemTable<Boot>) -> Status {
+    todo!();
 }
