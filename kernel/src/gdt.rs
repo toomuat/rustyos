0e9e88b718c72201e739e7220caf34d6d9391d43
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2021-09-24 10:30:43 +0900

    use lazy_static in GDT

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -1,17 +1,19 @@
+use lazy_static::lazy_static;
 use x86_64::instructions::segmentation::{Segment, CS, SS};
 use x86_64::instructions::tables::load_tss;
-use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
+use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
 use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtAddr;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
-static mut TSS: TaskStateSegment = TaskStateSegment::new();
-static mut GDT: GlobalDescriptorTable = GlobalDescriptorTable::new();
+// https://os.phil-opp.com/double-fault-exceptions/#the-ist-and-tss
+// Double Faults - The IST and TSS - Writing an OS in Rust Philipp Oppermann's blog
 
-pub fn initialize() {
-    unsafe {
-        TSS.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
+lazy_static! {
+    static ref TSS: TaskStateSegment = {
+        let mut tss = TaskStateSegment::new();
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
@@ -19,14 +21,41 @@ pub fn initialize() {
             let stack_end = stack_start + STACK_SIZE;
             stack_end
         };
+        tss
+    };
+}
 
-        let code_selector = GDT.add_entry(Descriptor::kernel_code_segment());
-        let data_selector = GDT.add_entry(Descriptor::kernel_data_segment());
-        let tss_selector = GDT.add_entry(Descriptor::tss_segment(&TSS));
-        GDT.load();
+struct Selectors {
+    pub kernel_code_selector: SegmentSelector,
+    pub kernel_data_selector: SegmentSelector,
+    pub tss_selector: SegmentSelector,
+}
 
-        CS::set_reg(code_selector);
-        SS::set_reg(data_selector);
-        load_tss(tss_selector);
+lazy_static! {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
+        let mut gdt = GlobalDescriptorTable::new();
+
+        let kernel_code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let kernel_data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+
+        (
+            gdt,
+            Selectors {
+                kernel_code_selector,
+                kernel_data_selector,
+                tss_selector,
+            },
+        )
+    };
+}
+
+pub fn initialize() {
+    GDT.0.load();
+
+    unsafe {
+        SS::set_reg(GDT.1.kernel_data_selector);
+        CS::set_reg(GDT.1.kernel_code_selector);
+        load_tss(GDT.1.tss_selector);
     }
 }
