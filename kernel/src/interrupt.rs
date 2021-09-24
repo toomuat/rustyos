6f1a0a8a0072ffe89bc2d0e3ceb1de5c77774814
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2021-09-24 10:37:15 +0900

    use lazy_static in IDT

--- kernel/src/interrupt.rs
+++ kernel/src/interrupt.rs
@@ -1,13 +1,18 @@
+use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
+
+        idt
+    };
+}
 
 pub fn init_idt() {
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.double_fault.set_handler_fn(double_fault_handler);
-        IDT.load();
-    }
+    IDT.load();
 }
 
 extern "x86-interrupt" fn double_fault_handler(
